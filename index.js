92afabb3cf5333cb7c2dd57333ccdb5f3e6e58ef
juangallard0 <00091813@uca.edu.sv>
2021-11-20 01:42:33 -0600

    Install crud template

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.listen(5000, function(){
     console.log("Server connected to port 5000");
 });
 
 app.get('/', (req, res) => {
-    res.send('test');
+    res.render('template.ejs');
 });
