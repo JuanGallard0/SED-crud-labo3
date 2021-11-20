031801f23357be2b81275881a626e329b36b8d27
juangallard0 <00091813@uca.edu.sv>
2021-11-20 01:53:31 -0600

    Post function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.listen(5000, function(){
-    console.log("Server connected to port 5000");
+  console.log("Server connected to port 5000");
 });
 
 app.get('/', (req, res) => {
-    res.render('template.ejs');
+  res.render('template.ejs');
+});
+
+app.post('/show', (req, res) => {
+  console.log(req.body);
 });
